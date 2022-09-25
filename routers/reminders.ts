927f1feff787ce812d5826a399ffd54d22823a62
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-09-25 13:01:37 +0530

    Built a simple api and modified the get and post end points.

--- routers/reminders.ts
+++ routers/reminders.ts
@@ -1,15 +1,20 @@
 import { Router } from "express";
+import Reminder from "../models/reminder";
 import CreateReminderDto from "./../dtos/create-reminders";
 
 const router = Router();
 
+const reminders: Reminder[] = [];
+
 router.get("/", (req, res) => {
-  res.send("List of reminders");
+  res.status(200).json(reminders);
 });
 
 router.post("/", (req, res) => {
   const { title } = req.body as CreateReminderDto;
-  res.json(title);
+  const reminder = new Reminder(title);
+  reminders.push(reminder); // storing. In real-world we use a database for this.
+  res.status(201).json(reminder);
 });
 
 export default router;
